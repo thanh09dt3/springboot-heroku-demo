23e3b68a7b897211676b6ff7e62fee301e4d9a30
thanh09dt3 <56309554+thanh09dt3@users.noreply.github.com>
2021-03-07 11:15:01 +0700

    1

--- src/main/java/com/example/demo/WelcomeController.java
+++ src/main/java/com/example/demo/WelcomeController.java
@@ -3,11 +3,14 @@ package com.example.demo;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.example.demo.dao.Student;
+
 @RestController
 	public class WelcomeController {
 
 	@GetMapping("/welcome")
-	public String welcome() {
-		return "Welcome to spring boot heroku demo Hương dở hơi";
+	public String welcome(Student stu) {
+		
+		return "Welcome to spring boot heroku demo Hương dở hơi " + stu.getName();
 	}
 }
---
+++ src/main/java/com/example/demo/dao/Student.java
@@ -0,0 +1,20 @@
+package com.example.demo.dao;
+
+public class Student {
+	public String name;
+	public String id;
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getId() {
+		return id;
+	}
+	public void setId(String id) {
+		this.id = id;
+	}
+	
+	
+}
